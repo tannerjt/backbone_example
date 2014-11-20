2adab483f9e576171540c617956bb7be9a1cbf67
Joshua Tanner <biolsurfer85@gmail.com>
2014-11-19 19:12:42 -0800

    Add states json and images

--- js/views.js
+++ js/views.js
@@ -1,7 +1,14 @@
 var GreetingView = Backbone.View.extend({
+	events : {
+		"click h2" : "sayGreeting"
+	},
+	template : _.template("<h2>Movie Name: </h2> <%= name %>"),
 	render : function () {
-		this.$el.html(this.model.get('name'));
+		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+	sayGreeting : function () {
+		alert(this.model.get('greeting'));
 	}
 });
 
